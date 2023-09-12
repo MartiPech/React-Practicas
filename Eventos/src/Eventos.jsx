56523fda84f55fa2d524acec2a8d900e98668197
MartiPech <christianmartipech@gmail.com>
2023-09-12 20:58:57 -0300

    nuevo aporte de los eventos , parte 1 , la parte 2 va a ser de los hook

--- Eventos/src/Eventos.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Eventos = () => {
-  return (
-    <div>Eventos</div>
-  )
-}
--- Eventos/src/main.jsx
+++ Eventos/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Eventos from './Eventos'
-  
+import {Contador} from './Contador'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Eventos />
+    < Contador value={0}/>
   </React.StrictMode>,
 )
