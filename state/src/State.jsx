6bde5aeba7439294c2e976f337af6211e0478629
MartiPech <christianmartipech@gmail.com>
2023-09-17 14:55:04 -0300

    State V3

--- state/src/State.jsx
+++ state/src/State.jsx
@@ -1,6 +1,23 @@
+function handlerClick (event){
+  console.log(event)
+  
+  
+} 
 
-export const State = () => {
-  return (
-    <div>State</div>
-  )
+export const State = ({value}) => {
+  const HandlerIncremento = () =>{
+      value += 1
+      console.log(value)    
+  }
+return (
+  
+  <>
+      <h1>State Contador: </h1>
+          <p>{value}</p>
+          <button onClick={function (){console.log("soy un boton")}}>Soy un boton</button> <br />
+          <button onClick={function(event){console.log(event)}}>info del click</button> <br />
+          <button onClick={handlerClick}>btn 3</button> <br />
+          <button onClick={HandlerIncremento}>Ingrementar</button>
+  </>
+)
 }
--- state/src/main.jsx
+++ state/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {State} from './state'
+import {State} from './State'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <State />
+    <State vualue = {0} />
   </React.StrictMode>,
 )
