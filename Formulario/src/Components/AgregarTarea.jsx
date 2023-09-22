bbeb668bf797730052d7e03faf108e799d5de11b
MartiPech <christianmartipech@gmail.com>
2023-09-22 20:29:10 -0300

    formulario v3

--- Formulario/src/Components/AgregarTarea.jsx
+++ Formulario/src/Components/AgregarTarea.jsx
@@ -2,11 +2,24 @@ import {useState} from 'react'
 
 export const AgregarTarea = () => {
   const [inputValue , setInputValue] = useState("")
+  const OnInputChange = (event)=>{
+    setInputValue(event.target.value)
+    
+  }
+  const onSubmit = (event)=>{
+    event.preventDefault()
+    console.log(inputValue)
+  }
 
   return (
     <>
-    <form action="">
-      <input type="text"placeholder='Ingrese tarea nueva' />
+    <form onSubmit={event =>onSubmit(event)}>
+      <input 
+      type="text"
+      placeholder='Ingrese tarea nueva'
+      value={inputValue}
+      onChange={(event)=> OnInputChange(event)} />
+  
     </form>
     </>
   )
