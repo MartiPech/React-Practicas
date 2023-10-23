e87cc4de5ce2b3d35499ecacaa0ad31e99afae6c
MartiPech <christianmartipech@gmail.com>
2023-10-22 23:46:08 -0300

    useCounter v2

--- useCounter/src/App.jsx
+++ useCounter/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+const useCounter = () => {
+    const [counter , setCounter] = useState(0)
+
+    const sumar =  () => setCounter(counter + 1)
+    const restar = () => setCounter(counter - 1)
+    const reset =  () => setCounter(0)
+
+    return{
+        counter,
+        sumar,
+        reset,
+        restar
+    }
+}
+
+export const App = () => {
+    const { counter, sumar, reset, restar } = useCounter();
+
+
+    return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Counter</h1>
+        <div> {counter} </div>
+            <button onClick={sumar}>Sumar</button>
+            <button onClick={reset}>Reset</button>
+            <button onClick={restar}>Restar</button>
     </>
   )
 }
-
-export default App
