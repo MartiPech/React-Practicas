172bfbfbeeffb2d3fe41e488c6fd91bd0efb3c55
MartiPech <christianmartipech@gmail.com>
2023-10-31 22:05:22 -0300

    Ejercicio practica v3 con ejemplos de los errors.nombre?type

--- Formularios hook/Practiva V3/reack-hook-form/src/Formularios.jsx
+++ Formularios hook/Practiva V3/reack-hook-form/src/Formularios.jsx
@@ -1,33 +1,48 @@
 import React from 'react';
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form';
+
 
 const Formularios = () => {
 
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit , formState:{errors}} = useForm();
+    const onSubmit = handleSubmit ((event) => console.log(event));
+    console.log(errors)
+    
+    
    
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={onSubmit}>
         {/*NOMBRE*/}
-        <label htmlFor="nombre">Nombre <input {...register('nombre')} type="text" /></label>
+        <label htmlFor="nombre">Nombre <input {...register('nombre',
+        {required:true,
+        minLength:2,
+        maxLength:20})} 
+        type="text" /></label>
         
+        {/* 
+        Esto es una forma para que salgan los errores de rellenario
+        {errors.nombre?.type ==="required" && <span>Nombre requerido</span>}
+        {errors.nombre?.type ==="minLength" && <span>Es muy corto</span>}
+        
+        */}
+    
+
         {/*EMAIL*/}
-        <label htmlFor="email">
-            Correo<input {...register('email')} type="email" />
-        </label>
+        <label htmlFor="email">Correo<input {...register('email',
+        {required:true})} type="email" /></label>
+        
         {/*CONTRASEÑA*/}
-        <label htmlFor="password">
-            Password <input {...register('password')} type="password" />
-        </label>
+        <label htmlFor="password">Password <input {...register('password',
+        {required:true})} type="password" /></label>
+        
         {/*CONFIRMAR CONTRASEÑA*/}
-        <label htmlFor="confirmarpassword">
-            Confirmar Password <input {...register('confirmarpassword')} type="password" />
-        </label>
+        <label htmlFor="confirmarpassword">Confirmar Password <input {...register('confirmarpassword',
+        {required:true})} type="password" /></label>
+        
         {/*date*/}
-        <label htmlFor="date">
-            Fecha de nacimiento <input {...register('date')} type="date" />
-        </label>
+        <label htmlFor="date">Fecha de nacimiento <input {...register('date')} type="date" /></label>
+        
         {/*pais*/}
         <label htmlFor="pais">
             Pais <select {...register("pais")} name="paises" >
@@ -37,13 +52,12 @@ const Formularios = () => {
                 <option value="UK">Rusia</option>
                 </select> 
         </label>
+        
         {/*Cargar Archivo*/}
-        <label htmlFor="file">
-            Foto <input {...register('file')} type="file" />
-        </label>
+        <label htmlFor="file">Foto <input {...register('file')} type="file" /></label>
+        
         {/*Termino de condición*/}
-        <label htmlFor="check">
-            Estoy de acuerdo <input {...register('checkbox')} type="checkbox" />
+        <label htmlFor="check">Estoy de acuerdo <input {...register('checkbox')} type="checkbox" />
             <button type='submit'>Enviar</button>
         </label>
 
