42687a92473a176ca147c6128359ae944333dac2
MartiPech <christianmartipech@gmail.com>
2023-11-03 22:13:08 -0300

    useRef

---
+++ useRef/src/HooksApps.jsx
@@ -0,0 +1,13 @@
+import FetchComponente from './components/FetchComponente'
+import {FormularioComponent} from './components/FormularioComponent'
+
+export const HooksApps = () => {
+  return (
+    <>
+    <h1>Aplicación hook</h1>
+    <hr />
+    <FormularioComponent></FormularioComponent>
+    <FetchComponente></FetchComponente>
+    </>
+  )
+}
