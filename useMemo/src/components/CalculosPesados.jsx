9f3a315a94676e6039483ad93e0924accf7f593f
MartiPech <christianmartipech@gmail.com>
2023-11-04 22:02:04 -0300

    useMEMOv2

--- useMemo/src/components/CalculosPesados.jsx
+++ useMemo/src/components/CalculosPesados.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
+const getCalculo =(listanumer)=>{
+    console.log("Calculando");
+    return listanumer.reduce((a,b)=>a*b)
+}
+
+
 export const CalculosPesados = () => {
-  return (
-    <h2>Calculos Pesados</h2>
-  )
+
+
+    const [show , SetShow] = useState(true)
+
+    const listanumer = [2,3,4,5,6,7,8,9]
+
+    return (
+        <>
+        <h2>Calculos Pesados</h2>
+        <p> {getCalculo(listanumer)} </p>
+        <button className="btn btn-primary">show</button>
+        </>
+    )
 }
