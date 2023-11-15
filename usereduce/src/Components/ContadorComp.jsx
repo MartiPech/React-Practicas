fac873bd1a8e15307e5375344d1f3f16a1e39901
MartiPech <christianmartipech@gmail.com>
2023-11-14 23:17:48 -0300

    Router DOM V1 : Estructura EMPTHY

--- usereduce/src/Components/ContadorComp.jsx
+++ usereduce/src/Components/ContadorComp.jsx
@@ -4,30 +4,30 @@ import { TYPES } from '../Helpers/CounterAction'
 
 export const ContadorComp = () => {
   
-  
   const [state, dispatch] = useReducer(CounterReducer, initialState)
-  
   
   const increment = ()=> dispatch({type:TYPES.INCREMENT})
   const increment_5 = ()=> dispatch({type:TYPES.INCREMENT_5,payload:5})
-
-  
   const decrementar = ()=> dispatch({type:TYPES.DECREMENT})
   const decrementar_5 = ()=> dispatch({type:TYPES.DECREMENT_5,payload:5})
   const reset = ()=> dispatch({type:TYPES.RESET})
+  
+  return (
+      <div className='father'>
+      <p>Contador {state.counter} </p>
+      <button onClick={increment}>+</button>
+      <button onClick={decrementar}>-</button>
+      <button onClick={increment_5}>+5</button>
+      <button onClick={decrementar_5}>-5</button>
+      <button onClick={reset}>Reset</button>
+      </div>
+      
+    )
+
+  
+  
 
 
   
   
-return (
-    <div className='father'>
-    <p>Contador {state.counter} </p>
-    <button onClick={increment}>+</button>
-    <button onClick={decrementar}>-</button>
-    <button onClick={increment_5}>+5</button>
-    <button onClick={decrementar_5}>-5</button>
-    <button onClick={reset}>Reset</button>
-    
-    </div>
-  )
 }
