bce7a8387a56ee7a03a3aca4175c4211ad09b4e8
MartiPech <christianmartipech@gmail.com>
2023-12-08 20:06:17 -0300

    pasito dos

--- Proyecto busqueda pelis/src/App.jsx
+++ Proyecto busqueda pelis/src/App.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const App = () => {
+  const urlBase ="'https://api.themoviedb.org/3/search/movie"
+  const ApiKey = "637751041a6e3e5274738b2d0b6fbb84"
+  
+  const handlerSubmit = (e)=>{
+    e.preventDefault()
+    fetchBusqueda()
+  }
+  
+  const handlerInput = (e) =>{
+      setBusqueda(e.target.value)
+  }
+
+
+  const fetchBusqueda = async() =>{
+    try{
+        const res = await fetch(`${urlBase}?query=${busqueda}&api_key=${ApiKey}`)
+        const data = await res.json()
+        setPelicula(data)
+        
+    }catch(error){
+      
+    }
+  }
+  
+  const [busqueda , setBusqueda] = useState("")
+  const [pelicula , setPelicula] = useState([])
+
   return (
-    <h1>Buscador de peliculas</h1>
-  )
+    <div className='container'>
+        <h1 className='title'>Buscador de peliculas</h1>
+          <form onSubmit={handlerSubmit}>
+            <input type="text"
+            placeholder='Escriba su pelicula'
+            onChange={handlerInput} />
+            <button>Buscar</button>
+          </form>
+    </div>
+
+    )
 }
